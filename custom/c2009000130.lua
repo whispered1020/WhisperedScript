cbc9da54f03d94777c0c35acffbce9022f4a02cc
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-07-29 15:20:28 -0300

    Updated Rose Beast

--- custom/c2009000130.lua
+++ custom/c2009000130.lua
@@ -5,63 +5,69 @@ function s.initial_effect(c)
     --Synchro Summon procedure
     Synchro.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,SET_ROSE),1,1,Synchro.NonTuner(nil),1,99)
     c:EnableReviveLimit()
-    --Change battle position
+    --add rose trap
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_POSITION)
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_ATTACK)
+    e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetCountLimit(1,id)
-    e1:SetTarget(s.postg)
-    e1:SetOperation(s.posop)
+    e1:SetCondition(function(e) return e:GetHandler():IsSynchroSummoned() end)
+    e1:SetTarget(s.thtg)
+    e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
-    --Destroy when battle position changes
+    --send plant/dragon monster to gy
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-    e2:SetCategory(CATEGORY_DESTROY)
-    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_CHANGE_POS)
+    e2:SetCategory(CATEGORY_TOGRAVE)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_MZONE)
     e2:SetCountLimit(1,{id,1})
-    e2:SetTarget(s.destg)
-    e2:SetOperation(s.desop)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
+    e2:SetCost(s.tgcost)
+    e2:SetTarget(s.tgtg)
+    e2:SetOperation(s.tgop)
     c:RegisterEffect(e2)
 end
 
+function s.thfilter(c)
+	return c:IsSetCard(SET_ROSE) and c:IsTrap() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
 --
-function s.postg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_MZONE) end
-    if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
-    local g=Duel.SelectTarget(tp,nil,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_POSITION,g,1,0,0)
+function s.rfilter(c)
+	return (c:IsRace(RACE_PLANT) or c:IsRace(RACE_DRAGON)) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
 end
-function s.posop(e,tp,eg,ep,ev,re,r,rp)
-    local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) and tc:IsLocation(LOCATION_MZONE) then
-        Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_DEFENSE,POS_FACEUP_ATTACK)
-        --Lock its position until end of turn
-        local e1=Effect.CreateEffect(e:GetHandler())
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-        tc:RegisterEffect(e1)
-    end
+function s.tgfilter(c)
+	return c:IsRace(RACE_PLANT) and c:IsAbleToGrave()
 end
---destroy
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-    local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+function s.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.rfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-    if #g>0 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-        local sg=g:Select(tp,1,1,nil)
-        Duel.Destroy(sg,REASON_EFFECT)
-    end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
 end
