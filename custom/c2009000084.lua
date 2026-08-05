ae0f812a5a3cbb382168931d5d50041fe5083cd6
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-08-05 14:24:07 -0300

    Fairy Princess OK

--- custom/c2009000084.lua
+++ custom/c2009000084.lua
@@ -1,110 +1,74 @@
---Rikka Princess Ascendant
+--Gentian the Rikka Fairy Princess
 --Scripted by: Whispered
 local s,id=GetID()
 function s.initial_effect(c)
-	--Link Summon procedure
-	c:EnableReviveLimit()
-	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_PLANT),2,nil,s.matcheck)
-	--Piercing Battle Damage
+	--draw 1 card
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_ATKCHANGE)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetCondition(s.pcon)
-    e1:SetOperation(s.pop)
+    e1:SetCategory(CATEGORY_DRAW)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
+    e1:SetCondition(s.drcon)
+    e1:SetCost(s.drcost)
+    e1:SetTarget(s.drtg)
+    e1:SetOperation(s.drop)
     c:RegisterEffect(e1)
-    --Add to hand from GY or attach to 1 Plant Xyz
+    --Special Summon "Rikka" from GY or banishment
 	local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOHAND)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_MZONE)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.thcon)
-	e2:SetCost(s.thcost)
-	e2:SetOperation(s.thop)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 end
 
-function s.matcheck(g,lc,sumtype,tp)
-	return g:IsExists(Card.IsSetCard,1,nil,0x141,lc,sumtype,tp)
+function s.confilter(c)
+    return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsRace(RACE_PLANT)
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.confilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    if chk==0 then return c:IsAbleToGraveAsCost() and (c:IsLocation(LOCATION_HAND) or c:IsFaceup()) end
+    Duel.SendtoGrave(c,REASON_COST)
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
 end
 --
-function s.pcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsLinkSummoned()
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(SET_RIKKA) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.pop(e,tp,eg,ep,ev,re,r,rp)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_COST) and re:IsActivated() and re:IsActiveType(TYPE_XYZ)
+		and c:IsPreviousLocation(LOCATION_OVERLAY)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+    local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    local g=Duel.GetMatchingGroup(Card.IsRace,tp,LOCATION_MZONE,0,nil,RACE_PLANT)
-    local tc=g:GetFirst()
-    while tc do
-        local e1=Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_SINGLE)
-        e1:SetCode(EFFECT_PIERCE)
-        e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-        tc:RegisterEffect(e1)
-        tc=g:GetNext()
-    end
-end
---
-function s.confilter(c,lc)
-    return c:IsReleasableByEffect() and lc:IsLinked(c)
-end
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    return Duel.IsExistingMatchingCard(s.confilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,c)
-end
-
-function s.cfilter(c,g)
-	return g:IsContains(c) and c:IsReleasable()
-end
-function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local lg=e:GetHandler():GetLinkedGroup()
-	local rg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,lg)
-	if chk==0 then return #rg>0 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local g=rg:Select(tp,1,1,nil)
-	Duel.Release(g,REASON_COST)
-end
-function s.thfilter(c)
-	return c:IsSetCard(SET_RIKKA) and c:IsAbleToHand()
-end
-function s.xyzfilter(c,e)
-    return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsRace(RACE_PLANT) and not c:IsImmuneToEffect(e)
-end
-function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil)
-            or Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil,e)
-    end
-    Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
-    Duel.SetPossibleOperationInfo(0,CATEGORY_ATTACH,nil,1,tp,LOCATION_GRAVE)
-end
-function s.thop(e,tp,eg,ep,ev,re,r,rp)
-    local b1=Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil)
-    local b2=Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil,e)
-    if not (b1 or b2) then return end
-    local op=Duel.SelectEffect(tp,
-        {b1,aux.Stringid(id,2)}, -- Add to hand
-        {b2,aux.Stringid(id,3)}) -- Attach as material
-    if op==1 then
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-        local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-        if #g>0 then
-            Duel.SendtoHand(g,nil,REASON_EFFECT)
-            Duel.ConfirmCards(1-tp,g)
-        end
-    else
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-        local xyz=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil,e):GetFirst()
-        if xyz then
-            Duel.HintSelection(xyz,true)
-            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-            local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-            if #g>0 then
-                Duel.Overlay(xyz,g)
-            end
-        end
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
     end
 end
