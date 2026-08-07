413a65d328f6280b2c1b6240c4b692d864b6120d
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-08-07 12:14:34 -0300

    Updated rikka cards

--- custom/c2009000082.lua
+++ custom/c2009000082.lua
@@ -4,82 +4,150 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Xyz Summon Procedure
-	Xyz.AddProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_PLANT),10,2,nil,nil,Xyz.InfiniteMats)
-	--negate and destroy
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_PLANT),10,2)
+	--detach 1 or more materials and apply the appropriate effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_CARD_TARGET)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
-	e1:SetCost(Cost.DetachFromSelf(1))
-	e1:SetTarget(s.negtg)
-	e1:SetOperation(s.negop)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
-	--change opponent's monsters to face-down defense position
+	--attach tributed monster to itself
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_POSITION)
-	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetDescription(aux.Stringid(id,3))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_RELEASE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCost(s.poscost)
-	e2:SetTarget(s.postg)
-	e2:SetOperation(s.posop)
+	e2:SetCondition(s.attachcon)
+	e2:SetTarget(s.attachtg)
+	e2:SetOperation(s.attachop)
 	c:RegisterEffect(e2)
+	--Negate chain 1 effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,4))
+	e3:SetCategory(CATEGORY_DISABLE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetCondition(s.discon)
+	e3:SetCost(s.discost)
+	e3:SetTarget(s.distg)
+	e3:SetOperation(s.disop)
+	c:RegisterEffect(e3)
 end
 
---Check for plant monster
-function s.cfilter(c,tp)
-	return c:IsRikkaReleasable(tp)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    local b1=c:CheckRemoveOverlayCard(tp,1,REASON_COST)
+        and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,nil)
+    local b2=c:CheckRemoveOverlayCard(tp,2,REASON_COST)
+        and Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil)
+    if chk==0 then return b1 or b2 end
+    local op
+    if b1 and b2 then
+        op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))+1
+    elseif b1 and not b2 then
+        op=1
+	elseif not b1 and b2 then
+		op=2
+    end
+    c:RemoveOverlayCard(tp,op,op,REASON_COST)
+    e:SetLabel(op)
 end
-function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and Card.IsNegatable(chkc) end
-	if chk==0 then return Duel.CheckReleaseGroup(tp,s.cfilter,1,nil,tp)
-		and Duel.IsExistingMatchingCard(Card.IsNegatable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
-	local g=Duel.SelectTarget(tp,Card.IsNegatable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-    Duel.SetOperationInfo(0,CATEGORY_NEGATE,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_RELEASE,nil,1,tp,LOCATION_MZONE)
+function s.thfilter(c)
+    return c:IsSetCard(0x141) and c:IsMonster() and c:IsAbleToHand()
 end
-function s.negop(e,tp,eg,ep,ev,re,r,rp)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then
+			return true 
+	end
+	local ct=e:GetLabel()
+	if ct==1 then  
+		--Add 1 "Rikka" monster from GY
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectTarget(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,1,nil)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,LOCATION_GRAVE)
+	elseif ct==2 then 
+		--return to hand 1 card your opponent controls
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,1-tp,LOCATION_ONFIELD)
+	end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetLabel()
+	if ct==1 then
+		-- Add 1 "Rikka" monster from GY
+		local tc=Duel.GetFirstTarget()
+		if tc and tc:IsRelateToEffect(e) then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		end
+	elseif ct==2 then
+		--return to hand 1 card your opponent controls
+		local tc=Duel.GetFirstTarget()
+		if tc and tc:IsRelateToEffect(e) then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		end
+	end
+end
+-- If a Plant monster(s) you control is Tributed: Attach 1 of those monsters
+function s.attfilter(c,tp)
+	return c:GetPreviousRaceOnField()&RACE_PLANT==RACE_PLANT and c:IsPreviousControler(tp)
+end
+function s.attachfilter(c,tp)
+	return c:GetPreviousRaceOnField()&RACE_PLANT==RACE_PLANT
+		and c:IsPreviousControler(tp)
+		and c:IsCanBeXyzMaterial()
+end
+function s.attachcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.attfilter,1,nil,tp)
+end
+function s.attachtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return eg:IsExists(s.attachfilter,1,nil,tp,e:GetHandler())
+	end
+end
+function s.attachop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local rg=Duel.SelectReleaseGroup(tp,s.cfilter,1,1,nil,tp)
-	local dg=Duel.GetFirstTarget()
-	if rg and Duel.Release(rg,REASON_EFFECT)~=0 and dg and dg:IsFaceup() and dg:IsRelateToEffect(e) then
-				Duel.NegateRelatedChain(dg,RESET_TURN_SET)
-				local e2=Effect.CreateEffect(c)
-				e2:SetType(EFFECT_TYPE_SINGLE)
-				e2:SetCode(EFFECT_DISABLE)
-				e2:SetReset(RESETS_STANDARD_PHASE_END)
-				dg:RegisterEffect(e2)
-				local e3=Effect.CreateEffect(c)
-				e3:SetType(EFFECT_TYPE_SINGLE)
-				e3:SetCode(EFFECT_DISABLE_EFFECT)
-				e3:SetValue(RESET_TURN_SET)
-				e3:SetReset(RESETS_STANDARD_PHASE_END)
-				dg:RegisterEffect(e3)
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local g=eg:Filter(s.attachfilter,nil,tp,c)
+	if #g==0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local tc=g:Select(tp,1,1,nil):GetFirst()
+	if tc then
+		Duel.Overlay(c,Group.FromCards(tc))
 	end
 end
---
-function s.posfilter(c)
-	return c:IsFaceup() and c:IsCanTurnSet()
+--negate chain 1
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain(true)>1 and Duel.IsChainDisablable(1)
 end
-function s.poscost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil,tp) end
-	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil,tp)
-	Duel.Release(g,REASON_COST)
-end
-function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(s.posfilter,tp,0,LOCATION_MZONE,nil)
-	if chk==0 then return #g>0 end
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,#g,0,0)
-end
-function s.posop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.posfilter,tp,0,LOCATION_MZONE,nil)
-	if #g>0 then
-		Duel.ChangePosition(g,POS_FACEDOWN_DEFENSE)
+
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return e:GetHandler():CheckRemoveOverlayCard(tp,3,REASON_COST)
 	end
+	e:GetHandler():RemoveOverlayCard(tp,3,3,REASON_COST)
+end
+
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local eff=Duel.GetChainInfo(1,CHAININFO_TRIGGERING_EFFECT)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eff:GetHandler(),1,0,0)
+end
+
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(1)
+	Duel.SetChainLimitTillChainEnd(aux.FALSE)
 end
--- custom/c2009000084.lua
+++ custom/c2009000084.lua
@@ -13,17 +13,16 @@ function s.initial_effect(c)
     e1:SetTarget(s.drtg)
     e1:SetOperation(s.drop)
     c:RegisterEffect(e1)
-    --Special Summon "Rikka" from GY or banishment
+    --Set "Rikka" Trap from GY
 	local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
     e2:SetCode(EVENT_TO_GRAVE)
     e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.spcon)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetCondition(s.setcon)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
 	c:RegisterEffect(e2)
 end
 
@@ -49,26 +48,32 @@ function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Draw(p,d,REASON_EFFECT)
 end
 --
-function s.spfilter(c,e,tp)
-    return c:IsSetCard(SET_RIKKA) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+function s.setfilter(c)
+    return c:IsSetCard(0x141) and c:IsTrap() and c:IsSSetable()
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+function s.setcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	return c:IsReason(REASON_COST) and re:IsActivated() and re:IsActiveType(TYPE_XYZ)
 		and c:IsPreviousLocation(LOCATION_OVERLAY)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-    if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil,e,tp) end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil,e,tp)
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+    local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,tp,LOCATION_GRAVE)
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local tc=Duel.GetFirstTarget()
-    if tc and tc:IsRelateToEffect(e) then
-        Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
-    end
+		if tc then
+			Duel.SSet(tp,tc)
+			Duel.ConfirmCards(1-tp,tc)
+			-- Allow activation this turn
+        	local e1=Effect.CreateEffect(c)
+        	e1:SetType(EFFECT_TYPE_SINGLE)
+        	e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+        	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+        	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        	tc:RegisterEffect(e1)
+		end
 end
