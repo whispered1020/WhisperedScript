d0f72f2063176cffeffb5c9ca7837d28df16a25c
Vinny Reis <38574940+whispered1020@users.noreply.github.com>
2026-09-23 10:01:03 -0300

    updated frigg

--- custom/c2009000158.lua
+++ custom/c2009000158.lua
@@ -32,7 +32,7 @@ function s.initial_effect(c)
 	e3:SetCode(EFFECT_UPDATE_ATTACK)
 	e3:SetRange(LOCATION_SZONE)
 	e3:SetTargetRange(LOCATION_MZONE,0)
-	e3:SetTarget(function(e,c) return c:IsSetCard(SET_NORDIC|SET_AESIR) end)
+	e3:SetTarget(function(e,c) return c:IsSetCard(SET_NORDIC) or c:IsSetCard(SET_AESIR) end)
 	e3:SetValue(300)
 	c:RegisterEffect(e3)
 	local e4=e3:Clone()
@@ -42,23 +42,29 @@ end
 s.listed_series={SET_NORDIC,SET_AESIR,SET_NORDIC_RELIC}
 
 function s.tgfilter(c)
-	return c:IsSetCard(SET_NORDIC_RELIC) and c:IsNormalSpellTrap() and c:IsAbleToGrave()
-end
-function s.tg2filter(c)
 	return c:IsSetCard(SET_NORDIC) and c:IsAbleToGrave()
 end
+function s.tg2filter(c)
+	return c:IsSetCard(SET_NORDIC_RELIC) and c:IsNormalSpellTrap() and c:IsAbleToGrave()
+end
+function s.tg3filter(c)
+	return c:IsSetCard(SET_NORDIC) and c:IsMonster() and c:IsAbleToGrave()
+end
 function s.rescon(sg)
-	return sg:FilterCount(Card.IsMonster,nil)==1
+	return sg:FilterCount(Card.IsMonster,nil)==1 and sg:IsExists(s.tg2filter,1,nil)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tg3filter,tp,LOCATION_DECK,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.tg2filter,tp,LOCATION_DECK,0,1,nil)
+	end
 	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,2,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local g=Duel.GetMatchingGroup(s.tgfilter,tp,LOCATION_DECK,0,nil)
 	local hg=Duel.GetMatchingGroup(s.tg2filter,tp,LOCATION_DECK,0,nil)
-	if #hg==1 then g:Sub(hg) end
-	if chk==0 then return #hg>0 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
+	local hg2=Duel.GetMatchingGroup(s.tg3filter,tp,LOCATION_DECK,0,nil)
+	if #hg==0 or #hg2==0 then return end
+	if chk==0 then return #hg>0 and #hg2>0 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0) end
 	local dg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_TOGRAVE)
 	    if #dg==2 then
 			Duel.SendtoGrave(dg,REASON_EFFECT)
